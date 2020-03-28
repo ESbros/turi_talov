2efa1d8251704967f3639f33924f5f47f2fdfd94
Erik Solis <41980160+ESbros@users.noreply.github.com>
2020-03-28 13:41:08 -0500

    Update train_sgn_model.py

--- train_sgn_model.py
+++ train_sgn_model.py
@@ -1,7 +1,10 @@
 import turicreate as tc
 
+sframe_dir = '/Users/erik/Desktop/signs/signs_csv+sframe/signs.sframe'
+
 # Load sessions from preprocessed data
-data = tc.SFrame('/Users/erik/Desktop/signs/signs_csv+sframe/signs.sframe')
+data = tc.SFrame(sframe_dir)
+save_dir = '/Users/erik/Desktop'
 
 print(data)
 
@@ -12,14 +15,14 @@ train, test = tc.activity_classifier.util.random_split_by_session(data,
 
 # Create an activity classifier
 model = tc.activity_classifier.create(train, session_id='exp_id', target='activity',
-                                      prediction_window=50, max_iterations=20)
+                                      prediction_window=50, max_iterations=5)
 
 # Evaluate the model and save the results into a dictionary
 metrics = model.evaluate(test)
 print(metrics)
 
 # Save the model for later use in Turi Create
-model.save('signs_detector.model')
+model.save(save_dir + '/' + 'signs_detector.model')
 
 # Export for use in Core ML
-model.export_coreml('/Users/erik/Desktop/signs_detector.mlmodel')
+model.export_coreml(save_dir + '/' + 'signs_detector.mlmodel')
